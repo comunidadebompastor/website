f0ccd76c8992a2f120f03aa134407fe25029d5d6
Denielly Scherrer Borges Menezes Casagrande <bompastor.ieab@gmail.com>
2023-10-25 18:54:13 -0300

    initial commit

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "A flake for developing and building my personal website";
+
+  inputs = {
+    flake-parts = { url = "github:hercules-ci/flake-parts"; inputs.nixpkgs-lib.follows = "nixpkgs"; };
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    theme = { url = "github:zeon-studio/themeplate"; flake = false; };
+    nix-filter.url = "github:numtide/nix-filter";
+  };
+
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+      imports = [
+        ./nix/website.nix
+        ./nix/development.nix
+      ];
+    };
+}
